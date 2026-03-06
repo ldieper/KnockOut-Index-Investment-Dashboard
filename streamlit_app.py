735425ea5788685316d392e4a63f7888f761e442
ldieper <louis_henri.dieper@smail.th-koeln.de>
2026-03-06 12:40:09 +0100

    First functional state (investing when 5%drop till knockout)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import altair as alt
 
 st.title("KnockOut-Investitions Simulation auf Indizes", anchor="title")
@@ -17,6 +16,7 @@ with bottom:
         "Hebel",
         ["3", "5", "10"]
     )
+    hebel = float(hebel)
 
     selected_index = st.radio(
         "Wähle einen Index aus:",
@@ -27,53 +27,104 @@ with bottom:
 with top:
     st.subheader(f"Kursverlauf - {selected_index}")
 
-    df_all_index = pd.read_csv(
-        "TestIndexRND.csv",
-        parse_dates=["zeit"], # Spalte ist ein Datum
-        dayfirst=True # Tag kommt zuerst. WICHTIG: Yfinance benutzt
+    df_all_index = pd.read_csv("TestIndexRND.csv")
+
+    df_all_index["zeit"] = pd.to_datetime(
+        df_all_index["zeit"],
+        format="%y-%m-%d"
     )
 
-    # Von Roh zu ausgewähltem Index
-    # st.write("Rohdaten")
-    # st.dataframe(df_all_index)
+    df_all_index = df_all_index[
+    df_all_index["index_name"] == selected_index
+    ].reset_index(drop=True)
 
-    df_selected_index = df_all_index[
-        df_all_index["index_name"] == selected_index]
-    
-    # Ausgabe gefilterter Indizes
-    # st.write("Daten von Ausgewähltem Index:")
-    # st.dataframe(df_selected_index) 
+    df_all_index["index_growth"] = df_all_index["index_wert"].pct_change().fillna(0)
+    df_all_index["index_growth_with_hebel"] = df_all_index["index_growth"] * hebel
+    df_all_index["index_investpoint"] = df_all_index["index_growth"] <= -0.05
+ 
 
-    chart = alt.Chart(df_selected_index)
+    df_all_index['current_invest_wert'] = 0.0
+    knockout_count = 0
 
-    line_index = chart.mark_line().encode(
+    aktive_investment = 0.0
+    is_invested = False        
+
+    for i in df_all_index.index:
+        if df_all_index.loc[i, "index_investpoint"] and not is_invested:
+            active_investment = df_all_index.loc[i, "index_wert"]
+            is_invested = True
+            df_all_index.loc[i, "current_invest_wert"] = active_investment
+            continue
+
+        if is_invested:
+            active_investment += df_all_index.loc[i, "index_growth_with_hebel"]
+            
+            if active_investment <= df_all_index.loc[i, "knockout"] and is_invested:
+                knockout_count += 1
+                active_investment = 0.0
+                is_invested = False
+            
+            
+            df_all_index.loc[i, "current_invest_wert"] = active_investment
+
+
+
+    # df_all_index.to_csv("TestIndexRND_filtered.csv", index=False) #Speichern der CSV
+    # st.write(df_all_index)
+
+
+    chart = alt.Chart(df_all_index)
+
+    line_index = chart.mark_line(color="aqua").encode(
        x=alt.X(
         "zeit:T",
         title="Zeit",
         axis=alt.Axis(
-            format="%d %b" # Tag/Monat als Format
+            format="%d %b"
             )
         ),
         y=alt.Y("index_wert:Q", title="Kurs")
     )
 
-    line_knockout = chart.mark_line().encode(
+    line_knockout = chart.mark_line(color="red").encode(
         x="zeit:T",
         y="knockout:Q"
     )
 
-    st.altair_chart(
-        (line_index + line_knockout)
+    line_invest = chart.mark_line(color="yellow").encode(
+        x="zeit:T",
+        y="current_invest_wert:Q"
     )
 
+    st.altair_chart(
+        (line_index + line_knockout + line_invest),
+        use_container_width=True
+    )
 
 
     st.subheader("Aktuelle Kennzahlen")
     
-    current_index_wert = df_selected_index.groupby("index_name")["index_wert"].last().round(3) # Letzter Wert bei GroupBy (gibt Serie an)
-    current_index_wert = float(current_index_wert[selected_index]) # Filterung, da noch Serie vorliegt
-    st.metric("Kurs", (current_index_wert), "Test")
 
-    current_knockout = df_selected_index["knockout"].iloc[-1] # Positionsindex df.iloc[zeile, spalte] (nicht bei Gruppen/ Serie)
-    current_knockout = float(round(current_knockout, 3))    
-    st.metric("KnockOut", current_knockout, "Test" )
+    col1, col2, col3, col4 = st.columns(4)
+
+    with col1: 
+        # Aktueller Kurs
+        current_index_wert = df_all_index.groupby("index_name")["index_wert"].last().round(3)
+        current_index_wert = float(current_index_wert[selected_index])
+        st.metric("Kurs", (current_index_wert), "Test")
+
+    with col2:
+        # Aktuelle KnockOut Grenze
+        current_knockout = df_all_index["knockout"].iloc[-1]
+        current_knockout = float(round(current_knockout, 3))    
+        st.metric("KnockOut", current_knockout, "Test" )
+
+    with col3:
+        # Buy In
+        first_index_wert = df_all_index.groupby("index_name")["index_wert"].first().round(3)
+        first_index_wert = float(first_index_wert[selected_index])
+        st.metric("Buy In", (first_index_wert), "Test")
+
+    with col4:
+        st.metric("Knockouts", knockout_count, "Test")
+  
