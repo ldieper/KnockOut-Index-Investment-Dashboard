c4987088179e20016279ff0f7db9ba8909c4df90
ldieper <louis_henri.dieper@smail.th-koeln.de>
2026-03-15 12:26:04 +0100

    Added loading file

--- yfinance_loader.py
+++ yfinance_loader.py
@@ -4,7 +4,7 @@ def download_last_week(tickers=["^GDAXI", "^GSPC", "XIN0.FGI"]):
     for ticker in tickers:
         df = yf.download(
             ticker,
-            period="7d",
+            period="7d", #7Tage
             interval="1d",
             auto_adjust=True
         )
