ec20340b96f4c5e0fb74a80711c2741a0f195c7b
ldieper <louis_henri.dieper@smail.th-koeln.de>
2026-03-15 12:22:51 +0100

    First pull from yfinance

---
+++ yfinance_loader.py
@@ -0,0 +1,25 @@
+import yfinance as yf
+
+def download_last_week(tickers=["^GDAXI", "^GSPC", "XIN0.FGI"]):
+    for ticker in tickers:
+        df = yf.download(
+            ticker,
+            period="7d",
+            interval="1d",
+            auto_adjust=True
+        )
+
+        df.index = df.index.strftime('%Y-%m-%d')
+        df = df[["Close"]]
+
+        df.to_json(
+            f"yfinance_indizes/{ticker}_last_week.json",
+            orient="index",
+            date_format="iso",
+            indent=2
+        )
+
+        print(df.tail())
+
+
+download_last_week()
