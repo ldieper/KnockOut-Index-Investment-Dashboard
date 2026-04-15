03882b066d0e92d25677586ed365feb42bad8f85
ldieper <louis_henri.dieper@smail.th-koeln.de>
2026-04-15 16:43:04 +0200

    some changes for performance

---
+++ investment_simulation.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas as pd
+from investment import Investment
+
+
+#def get_comulative_investment_value(investments_list):
+#    total_value = 0
+#    for inv in investments_list:
+#        if inv.active:
+#            total_value += inv.get_investment_value()
+#    return total_value
+
+@st.cache_data
+def run_simulation(source, filter, selected_hebel, selected_budget, remaining_budget):
+    df = source.copy()
+
+    rows = [] #Zum unterscheiden der Reihen, der Investments
+    investments_list = [] #Liste der Klassen von Investment
+    investment_count = 0 #Zählt die Anzahl an Investment und wird für die Vergabe der inv.id verwendet
+    cumulative_investment_value = 0 #Kumulativer Wert aller Investments, um die Rendite und aktuelleb Stand zu berechnen
+
+    for i in df[filter].index:
+
+        if i%20 == 0: #Alle 20 Handelstage (ca. 1 Monat umgerechnet) wird das monatliche Budget erhöht
+            remaining_budget += selected_budget
+
+        if df.loc[i, "index_investpoint"]:
+            
+            investment_count += 1
+            new_inv = Investment(source=df, #Erstellen eines neuen Investments
+                            i=i,
+                            selected_hebel=selected_hebel,
+                            selected_budget=selected_budget,
+                            remaining_budget=remaining_budget,
+                            inv_id=investment_count) #ID des Investments == Invest_counter
+            investments_list.append(new_inv) #Hinzufügen zu investments Liste
+            new_inv.start_investment() #Startet das Investment
+            remaining_budget -= new_inv.get_investment_value() #Abziehen des Investments vom Budget
+
+        for inv in investments_list:
+
+            if not inv.active: #Falls das Investment bereits inaktiv ist zu diesem Zeitpunkt
+                continue
+
+            inv.update_current_knockout_barrier(i=i) #Aktualisiert die Knockoutbarriere
+            inv.update_investment_value(i=i) #Aktualisiert den aktuellen Wert des Investments
+            inv.update_hebel(i=i) #Aktualisiert den Hebel des Investments
+            inv.update_gewinn() #Aktualisiert die Rendite des Investments
+
+            #Fall eines Knockouts, berechnet aus Hebel
+            if  inv.get_hebel() == 0:
+                inv.reset_investment(type="knockout")
+                continue
+
+            #Fall eines Knockouts, berechnet aus aktuellem Wert des Investments
+            if inv.get_investment_value() <= 0:
+                inv.reset_investment(type="knockout")
+                continue
+
+            #Fall eines regulären Verkaufs, weil der Hebel unter 1.5x gefallen ist
+            if inv.get_hebel() <= 1.5:
+                inv.reset_investment(type="sell")
+                continue
+
+            value = inv.get_investment_value()
+            cumulative_value += value
+
+            # speichern der Werte in zuordbaren Reihen
+            rows.append({
+                "date": df["date"].loc[i],
+                "inv_id": inv.id,
+                "knockout_barrier": inv.get_current_knockout_barrier(),
+                "current_value": value,
+                "hebel": inv.get_hebel(),
+                "gewinn": inv.get_gewinn(),
+                "closing_reason": inv.closing_reason,
+                "starting_investment": inv.starting_investment,
+                "active": inv.active,
+            })
+    df_investment = pd.DataFrame(rows) #Dataframe aus den gesammelten Reihen der Investments
+
+    return investments_list, remaining_budget, df_investment
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,26 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-from investment import Investment
+from investment_simulation import run_simulation
 
 st.set_page_config(layout="wide")
 
 st.title("KnockOut-Investition auf Indizes")
 
 top = st.container(border=True)
-mid = st.container(border=True)
 bottom = st.container(border=True)
 
 
-with bottom:
-    
-    st.subheader("Aktuelle Regler")
+if "selected_index" not in st.session_state:
+    st.session_state.selected_index = "DAX"
 
-    col1, col2, col3 = st.columns(3)
+if "selected_hebel" not in st.session_state:
+    st.session_state.selected_hebel = 3.0
 
-    with col1:
-        selected_index = st.radio(
-            "Index",
-            ["DAX", "S&P 500", "FSTE China 50"]
-        )
 
-    with col2:  
-        selected_hebel = st.radio(
-            "Hebel",
-            ["3", "5", "10"]
-        )
-        selected_hebel = float(selected_hebel)
-
-    with col3:  
-        investment_break = st.slider("Wähle die Anzahl der Handels-Tage, die zwischen zwei Investments liegen soll",
-                        min_value=5,
-                        max_value=252,
-                        value=200,)
-        investment_break = int(investment_break)
-        
 
 with top:
-    st.subheader(f"Kursverlauf - {selected_index}")
+    st.subheader(f"Kursverlauf - {st.session_state.selected_index}")
 
     index_map = {
         "DAX": "yfinance_indizes/^GDAXI.json",
@@ -48,7 +28,7 @@ with top:
         "FSTE China 50": "yfinance_indizes/^HSI.json"
     }
 
-    selected_file = index_map[selected_index]
+    selected_file = index_map[st.session_state.selected_index]
     df_yf = pd.read_json(selected_file, orient="index")
 
     df_yf.index = pd.to_datetime(df_yf.index)
@@ -76,7 +56,6 @@ with top:
     remaining_budget = selected_budget
 
 
-
     df_all_index["yearly_high"] = (
         df_all_index["index_wert"]
             .rolling(window=252, min_periods=1)  # ~trading days in a year
@@ -91,95 +70,71 @@ with top:
         price = df_all_index.loc[i, "index_wert"]
         high = df_all_index.loc[i, "yearly_high"]
 
+        if df_all_index["index_investpoint"].sum() == 0:
+            if price < high * 0.9: #Erster Investmentpunkt, wenn Kurs unter 90% des 52-Wochen-Hochs fällt
+                df_all_index.loc[i, "index_investpoint"] = True
+                last_investment_point_value = df_all_index.loc[i, "index_wert"]
+                continue
+
         if price < high * 0.9:
-            if not df_all_index["index_investpoint"].iloc[max(0, i-investment_break):i].any():  # Kein Investment im letzten Monat (20 Handelstage)
-                df_all_index.loc[i, "index_investpoint"] = True
+            #if investment_break_type == "20 Tage":
+                if not df_all_index["index_investpoint"].iloc[max(0, i-20):i].any():  
+                    df_all_index.loc[i, "index_investpoint"] = True
+                    last_investment_point_value = df_all_index.loc[i, "index_wert"]
+                    continue
 
-        if price < high * 0.8:
-             if df_all_index["index_investpoint"].iloc[max(0, i-investment_break):i].sum() < 2 and 0 < df_all_index["index_investpoint"].iloc[max(0, i-investment_break):i].sum():  # Investment im letzten Monat (20 Handelstage)
-                df_all_index.loc[i, "index_investpoint"] = True
+            #elif investment_break_type == "Bis Wiederanstieg auf gleichen Wert" and df_all_index.loc[i, "index_wert"] > last_investment_point_value: 
+            #    df_all_index.loc[i, "index_investpoint"] = True
+            #    last_investment_point_value = df_all_index.loc[i, "index_wert"]
+            #    continue
+
+
+    #Sim-Run
+    df_investment, remaining_budget = run_simulation(
+        df_all_index,
+        mask,
+        st.session_state.selected_hebel,
+        selected_budget,
+        remaining_budget
+    )
     
-    rows = [] #Zum unterscheiden der Reihen, der Investments
-    investments_list = [] #Liste der Klassen von Investment
-    investment_count = 0 #Zählt die Anzahl an Investment und wird für die Vergabe der inv.id verwendet
-    cumulative_investment_value = 0 #Kumulativer Wert aller Investments, um die Rendite und aktuelleb Stand zu berechnen
-
-    def get_comulative_investment_value(i):
-        total_value = 0
-        for inv in investments_list:
-            if inv.active:
-                total_value += inv.get_investment_value()
-        return total_value
-
-    for i in df_all_index[mask].index:
-
-        if i%20 == 0: #Alle 20 Handelstage (ca. 1 Monat umgerechnet) wird das monatliche Budget erhöht
-            remaining_budget += selected_budget
-
-        if df_all_index.loc[i, "index_investpoint"]:
-            
-            investment_count += 1
-            new_inv = Investment(source=df_all_index, #Erstellen eines neuen Investments
-                         i=i,
-                         selected_hebel=selected_hebel,
-                         selected_budget=selected_budget,
-                         remaining_budget=remaining_budget,
-                         inv_id=investment_count) #ID des Investments == Invest_counter
-            investments_list.append(new_inv) #Hinzufügen zu investments Liste
-            new_inv.start_investment() #Startet das Investment
-            remaining_budget -= new_inv.get_investment_value() #Abziehen des Investments vom Budget
-
-        for inv in investments_list:
-
-            if not inv.active: #Falls das Investment bereits inaktiv ist zu diesem Zeitpunkt
-                continue
-
-            
-
-            inv.update_current_knockout_barrier(i=i) #Aktualisiert die Knockoutbarriere
-            inv.update_investment_value(i=i) #Aktualisiert den aktuellen Wert des Investments
-            inv.update_hebel(i=i) #Aktualisiert den Hebel des Investments
-            inv.update_gewinn() #Aktualisiert die Rendite des Investments
-
-            #Fall eines Knockouts, berechnet aus Hebel
-            if  inv.get_hebel() == 0:
-                inv.reset_investment(type="knockout")
-                continue
-
-            #Fall eines Knockouts, berechnet aus aktuellem Wert des Investments
-            if inv.get_investment_value() <= 0:
-                inv.reset_investment(type="knockout")
-                continue
-
-            #Fall eines regulären Verkaufs, weil der Hebel unter 1.5x gefallen ist
-            if inv.get_hebel() <= 1.5:
-                inv.reset_investment(type="sell")
-                continue
-
-            # speichern der Werte in zuordbaren Reihen
-            rows.append({
-                "date": df_all_index.loc[i, "date"],
-                "inv_id": inv.id,
-                "knockout_barrier": inv.get_current_knockout_barrier(),
-                "current_value": inv.get_investment_value(),
-                "hebel": inv.get_hebel(),
-                "gewinn": inv.get_gewinn(),
-                "cumulative_investment_value": get_comulative_investment_value(i),
-            })
-
     
-    discarded_count = sum(1 for inv in investments_list if inv.closing_reason == 2) #Summe der fehlerhaften Investments, bei denen nicht genug Geld für den Kauf einer Option vorhanden war
-    trades_count = sum(1 for inv in investments_list if not inv.closing_reason == 2) #Anzahl an trades (ohne fehlerhafte, ohne genug Geld)
-    active_trades  = sum(1 for inv in investments_list if inv.active)
-    closed_trades = sum(1 for inv in investments_list if not inv.active)
-    sells_count = sum(1 for inv in investments_list if inv.closing_reason == 1) #True = Sell
-    knockouts_count = sum(1 for inv in investments_list if inv.closing_reason == 0) #False = Knockout
-    not_enough_money_count = sum(1 for inv in investments_list if inv.closing_reason == 2) #Not enough money
-    loss_sum = sum(inv.starting_investment for inv in investments_list if inv.closing_reason == 0) #Summe der Verluste durch Knockout
-    profit_sum = sum(inv.get_gewinn() for inv in investments_list if inv.closing_reason != 2) #Summe der Gewinne durch regulären Verkauf
+    #discarded_count = sum(1 for inv in investments_list if inv.closing_reason == 2) #Summe der fehlerhaften Investments, bei denen nicht genug Geld für den Kauf einer Option vorhanden war
+    #trades_count = sum(1 for inv in investments_list if not inv.closing_reason == 2) #Anzahl an trades (ohne fehlerhafte, ohne genug Geld)
+    #active_trades  = sum(1 for inv in investments_list if inv.active)
+    #closed_trades = sum(1 for inv in investments_list if not inv.active)
+    #sells_count = sum(1 for inv in investments_list if inv.closing_reason == 1) #True = Sell
+    #knockouts_count = sum(1 for inv in investments_list if inv.closing_reason == 0) #False = Knockout
+    #not_enough_money_count = sum(1 for inv in investments_list if inv.closing_reason == 2) #Not enough money
+    #loss_sum = sum(inv.starting_investment for inv in investments_list if inv.closing_reason == 0) #Summe der Verluste durch Knockout
+    #profit_sum = sum(inv.get_gewinn() for inv in investments_list if inv.closing_reason != 2) #Summe der Gewinne durch regulären Verkauf
+    #final_gewinn = round(profit_sum - loss_sum, 2)
+    #total_invested_sum = sum(inv.starting_investment for inv in investments_list if inv.closing_reason != 2) #Summe des investierten Kapitals (ohne fehlerhafte Investments)
+    #total_rendite = round(final_gewinn / total_invested_sum * 100, 2) if total_invested_sum > 0 else 0 #Rendite in Prozent
+
+    discarded_count = (df_investment["closing_reason"] == 2).sum()
+    trades_count = (df_investment["closing_reason"] != 2).sum()
+    active_trades = df_investment["active"].sum()
+    closed_trades = (~df_investment["active"]).sum()
+    sells_count = (df_investment["closing_reason"] == 1).sum()
+    knockouts_count = (df_investment["closing_reason"] == 0).sum()
+    not_enough_money_count = (df_investment["closing_reason"] == 2).sum()
+
+    loss_sum = df_investment.loc[
+        df_investment["closing_reason"] == 0,
+        "starting_investment"
+    ].sum()
+    profit_sum = df_investment.loc[
+        df_investment["closing_reason"] != 2,
+        "gewinn"
+    ].sum()
     final_gewinn = round(profit_sum - loss_sum, 2)
-    total_invested_sum = sum(inv.starting_investment for inv in investments_list if inv.closing_reason != 2) #Summe des investierten Kapitals (ohne fehlerhafte Investments)
-    total_rendite = round(final_gewinn / total_invested_sum * 100, 2) if total_invested_sum > 0 else 0 #Rendite in Prozent
+    total_invested_sum = df_investment.loc[
+        df_investment["closing_reason"] != 2,
+        "starting_investment"
+    ].sum()
+    total_rendite = round(final_gewinn / total_invested_sum * 100, 2) if total_invested_sum > 0 else 0
+
 
     df_investment = pd.DataFrame(rows) #Dataframe aus den gesammelten Reihen der Investments
     df_all_index["date"] = pd.to_datetime(df_all_index["date"])
@@ -234,31 +189,65 @@ with top:
     st.altair_chart(combined_chart, width="stretch")           
 
 
-with mid:
+with bottom:
+    with st.container(border=False):
 
-    st.subheader("Aktuelle Kennzahlen")
-    
+        bottom_left, bottom_right = st.columns([0.7, 0.3])
 
-    col1, col2, col3, col4, col5 = st.columns(5)
+        with bottom_left:
+            with st.container(border=True):
 
-    with col1:
-        st.metric("Investment-Kurs", f"€ {round(rows[-1]['cumulative_investment_value'], 2):,.2f}".replace(",", " "), "Test")
-        current_index_wert = float(df_all_index["index_wert"].iloc[-1])
-        st.metric("Index-Kurs", f"{current_index_wert:,.3f}".replace(",", " "), "Test")
+                st.subheader("Aktuelle Kennzahlen")
+                
 
-    with col2:
-        st.metric("Gewinn", f"€ {final_gewinn:,.2f}".replace(",", " "), "Test")
-        st.metric("Verkäufe (Hebel < 1.5x)", f"{sells_count}", "Test")
+                col1, col2, col3, col4, col5 = st.columns(5)
 
-    with col3:
-        st.metric("Verluste", f"€ {round(loss_sum, 2):,.2f}".replace(",", " "), "Test")
-        st.metric("KnockOut", f"{knockouts_count}", "Test" )
+                with col1:
+                    st.metric("Investment-Kurs", f"€ {round(rows[-1]['cumulative_investment_value'], 2):,.2f}".replace(",", " "), "Test")
+                    current_index_wert = float(df_all_index["index_wert"].iloc[-1])
+                    st.metric("Index-Kurs", f"{current_index_wert:,.3f}".replace(",", " "), "Test")
 
-    with col4:
-        st.metric("Rendite", f"{total_rendite} %", "Test")
-        st.metric("Anzahl aktiver Investments", f"{active_trades}", "Test")
-        #st.metric("Fehlerhafte Investments (kein Geld)", f"{not_enough_money_count}", "Test")
+                with col2:
+                    st.metric("Gewinn", f"€ {final_gewinn:,.2f}".replace(",", " "), "Test")
+                    st.metric("Verkäufe (Hebel < 1.5x)", f"{sells_count}", "Test")
 
-    with col5:
-        st.metric("Verfügbares Budget", f"€ {remaining_budget:,.2f}".replace(",", " "), "Test")
-        st.metric("Monatliches Budget", f"€ 500,00", "Test")
+                with col3:
+                    st.metric("Verluste", f"€ {round(loss_sum, 2):,.2f}".replace(",", " "), "Test")
+                    st.metric("KnockOut", f"{knockouts_count}", "Test" )
+
+                with col4:
+                    st.metric("Rendite", f"{total_rendite} %", "Test")
+                    st.metric("Anzahl aktiver Investments", f"{active_trades}", "Test")
+                    #st.metric("Fehlerhafte Investments (kein Geld)", f"{not_enough_money_count}", "Test")
+
+                with col5:
+                    st.metric("Verfügbares Budget", f"€ {remaining_budget:,.2f}".replace(",", " "), "Test")
+                    st.metric("Monatliches Budget", f"€ 500,00", "Test")
+
+        with bottom_right:
+            with st.container(border=True):
+
+                st.subheader("Einstellungen")
+
+                col1, col2 = st.columns(2)
+
+                with col1:
+                    st.radio(
+                        "Index",
+                        ["DAX", "S&P 500", "FSTE China 50"],
+                        key="selected_index"
+                    )
+
+                with col2:
+                    st.radio(
+                        "Hebel",
+                        [3, 5, 10],
+                        key="selected_hebel"
+                    )
+
+                #with col3:  
+                #   st.metric
+                #    investment_break_type = st.radio(
+                #        "Wähle die Dauer der Investitionsperiode",
+                #       ["20 Tage", "Bis Wiederanstieg auf gleichen Wert"]
+                #    )
